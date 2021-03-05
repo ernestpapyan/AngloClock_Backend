2485b66945431ea90e8708556cd9dbbb41195cbf
ernestpapyan <62955095+ernestpapyan@users.noreply.github.com>
2021-03-05 09:45:48 +0400

    login api added

--- config/db.config.js
+++ config/db.config.js
@@ -2,17 +2,11 @@
 //     HOST: "156.67.212.107",
 //     USER: "u3717248_asile",
 //     PASSWORD: "asile123!@#",
-//     DB: "u3717248_asile"
+//     DB: "u3717248_asile_so"
 //   };
 module.exports = {
-    HOST: "156.67.212.107",
-    USER: "u3717248_asile",
-    PASSWORD: "asile123!@#",
-    DB: "u3717248_asile_so"
-  };
-// module.exports = {
-//   HOST: "localhost",
-//   USER: "root",
-//   PASSWORD: "",
-//   DB: "asile"
-// };
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "",
+    DB: "angloclock"
+};
